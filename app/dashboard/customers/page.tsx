a4d72bf314934cac80b487e4292e38ada7c6c451
DanyloAksonov <danyl.aksonov@sysgears.com>
2023-12-13 10:56:46 +0200

    Added dashboard simple app.

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const CustomersPage: NextPage = () => {
+  return <div>Customers Page</div>;
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const InvoicesPage: NextPage = () => {
+  return <div>Invoices Page</div>;
+};
+
+export default InvoicesPage;
